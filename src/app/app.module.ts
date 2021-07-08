bec9261330535bb87705b0f3d2d4903d6119f4bb
MiaJLee <angella555@naver.com>
2021-07-08 19:03:37 +0900

    delete: all code related to section 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
-import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent,
-    ServerComponent,
   ],
   imports: [
     BrowserModule,
