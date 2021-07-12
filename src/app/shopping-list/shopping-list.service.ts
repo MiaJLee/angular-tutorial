b99132340df29205ea19a5897230efdf845c759d
MiaJLee <angella555@naver.com>
2021-07-12 23:36:53 +0900

    prac: using services for pushing data from A to B - subscribe eventEmitter

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
-export class ShoppingListService {
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5), 
+    new Ingredient('Sugar', 4)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredients(ingredients: Ingredient){
+    this.ingredients.push(ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
